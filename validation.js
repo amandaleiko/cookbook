3d998edc60c2476291905a6fdf688cdfe962a3e4
Amanda <amandaleiko123@gmail.com>
2020-07-06 10:50:33 -0400

    add javascript

---
+++ validation.js
@@ -0,0 +1,26 @@
+//email address validation//
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
+
+//password validation//
+function CheckPassword(inputtxt) 
+{ 
+var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+if(inputtxt.value.match(passw)) 
+{ 
+alert('Good password')
+return true;
+}
+else
+{ 
+alert('Please enter a password that is a minimum for 6 characters, with at least one number, an uppercase and lowercase letter.')
+return false;
+}
+}
